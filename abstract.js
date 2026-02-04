518d8cc38ae072da8fa7cbf58c407e3adf15198c
Michael7217 <michaelcharlys555@gmail.com>
2026-02-03 22:15:41 -0300

    aula adicionada

---
+++ abstract.js
@@ -0,0 +1,92 @@
+//metodo ter ações diferentes
+class padraocarro{
+    constructor(){
+        //classe nao pode ser instanciada
+        //abstract
+        if(this.constructor === padraocarro){
+            throw new TypeError("esta classe não pode ser instanciada");
+            }
+        if (this.ligar === undefined) {
+            throw new TypeError("é obrigatorio implementar o metodo ligar")
+        }
+        if (this.desligar === undefined) {
+            throw new TypeError("é obrigatorio implementar o metodo desligar")
+        }
+        this.rodas=4
+        this.portas=4
+        this.ligado=false
+    }
+}
+
+
+
+class carro extends padraocarro{
+    constructor(tipo, estturbo){
+        super()
+        this.turbo=new turbo(estturbo)
+        if(tipo == 1){
+            this.velmax=120
+            this.nome='normal'
+        }else if(tipo == 2){
+            this.velmax=160
+            this.nome='esportivo'
+        }else if(tipo == 3){
+            this.velmax=200
+            this.nome='super esportivo'
+        }
+        this.velmax+=this.turbo.pot
+    }
+    info(){
+        console.log(this.nome)
+        console.log(this.velmax)
+        console.log(this.turbo.pot)
+    }
+    ligar(){
+        this.ligado=true
+    }
+    desligar(){
+        this.ligado=false
+    }
+}
+
+class turbo{
+    constructor(e){
+        if(e == 0){
+            this.pot=0
+        }else if(e == 1){
+            this.pot=50
+        }else if(e == 2){
+            this.pot=75
+        }else{
+            this.pot=100
+        }
+    }
+}
+
+
+class carroespecial extends carro{
+    constructor(estturbo){
+        super(4, estturbo)
+        this.tipoinfo=0
+        this.velmax=300+this.turbo.pot
+        this.nome='carro especial'
+    }
+    info(){
+        /* super.info() */
+        if(this.tipoinfo == 1){
+            super.info() //chama o metodo da classe base
+        }else{
+            console.log(`nome: ${this.nome}`)
+            console.log(`velmax: ${this.velmax}`)
+            console.log(`turbo: ${this.turbo.pot}`)
+        }
+    }
+}
+const c1 = new carro(1,0)
+const c2 = new carro(1,1)
+const c3 = new carroespecial(3)
+
+
+c1.info()
+c2.info()
+c3.info()
